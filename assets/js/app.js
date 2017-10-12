e3dda224624edd12b13a14e20028366274f523ab
Lindsay Wall <lindsay.is@gmail.com>
2017-10-11 20:31:46 -0400

    adding color text object

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,6 +85,7 @@ $(".submit").on("click", function(event) {
     var results = response.forecast.txt_forecast.forecastday[0];
     var zippy = $("<p class='zippo'>");
     var weatherInfo = $("<p>");
+    var highTemp = response.forecast.simpleforecast.forecastday[0].high.fahrenheit;
     console.log(results.fcttext);
 
     $(".weather").append(zippy);
@@ -99,7 +100,6 @@ $(".submit").on("click", function(event) {
 
 function weather() {
   if (localStorage.getItem("zip").length === 5) {
-    console.log("running");
     var zip = $(".zippy").val().trim();
     var queryURL = "https://api.wunderground.com/api/b6005ea6b47964f3/forecast/geolookup/q/" + localStorage.getItem("zip") + ".json";
     
@@ -112,12 +112,11 @@ function weather() {
     var zippy = $("<p class='zippo'>");
     var weatherInfo = $("<p>");
     var weatherIcon = response.forecast.txt_forecast.forecastday[0].icon;
+    var highTemp = response.forecast.simpleforecast.forecastday[0].high.fahrenheit;
     getABeer(weatherIcon);
-    console.log(results.fcttext);
 
     $(".weather").append(zippy);
     zippy.text(response.location.zip);
-    console.log(response.location.zip);
     $(".weather").append(weatherInfo);
     weatherInfo.text(results.fcttext);
     })
@@ -138,7 +137,6 @@ function getABeer(val1) {
   var randomBeerType = Math.floor(Math.random() * recommendationsObj.pairs[weatherKey].beer.length)
   var APIkey = "c54928017d8919c3c993272329ea38d1";
   var beer = recommendationsObj.pairs[weatherKey].beer[randomBeerType];
-  console.log("line 106 I think the beer is " + beer);
   var searchQueryURL = "https://api.brewerydb.com/v2/search?key=" + APIkey + "&q=" + beer + "&type=beer&withBreweries=Y";
 
   $.ajax({
@@ -146,9 +144,7 @@ function getABeer(val1) {
     method: "GET"
   }).done(function(cheese) {
     var randomBeerArrNum = Math.floor(Math.random() * 50);
-    console.log(randomBeerArrNum);
     console.log(cheese);
-
 
     var beerName = cheese.data[randomBeerArrNum].name;
     var brewery = cheese.data[randomBeerArrNum].breweries[0].name;
@@ -190,14 +186,101 @@ function getABeer(val1) {
     beerLabel.attr("src", label).addClass("img-responsive beer-label");
     $(".beer").append(beerLabel);
 
-    console.log("line 117 " + description);
-    console.log(beerName);
-    console.log(label);
-    console.log(abv);
-    console.log(beerBrewery)
-
     sendBeerToFire(beerName, description, abv, brewery);
   })
+}
+
+function getAColorText(temp) {
+  var weatherText 
+
+  switch(temp) {
+    case temp >= 90: 
+      var colorTextArr = ["Today's weather is sweaty armpits and misery", 
+      "Be prepared to melt like the wicked witch you know you secretly are.", 
+      "Today’s weather is fried eggs on the sidewalk. They’re eco-friendly, I guess.",
+      "The human body is made of 65% water and that’s way less than the humidity today. Good luck.",
+      "Today’s weather is just fucking miserable.",
+      "Wow, this is perfect weather for a beer. Like yesterday. And tomorrow. And forever. Every day is perfect for a beer.",
+      "Today’s weather is like you smoked a bunch of bath salts in Florida but you don’t know where you got the bath salts or how you got to Florida.",
+      "Mordor",
+      "These are the days sweat stains are made out of.",
+      "Today you’re going to sweat in places you didn’t know could sweat. It’s going to be awful."]
+
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 80 && < 90: 
+      var colorTextArr = ["Today is undercooked fried eggs on the sidewalk. You’re still going to sweat.",
+      "Today is...still too hot."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 70 && < 90:
+      var colorTextArr = ["Is it Spring? Is it Fall? Is it that weird winter day that reminds you global warming is real? I don’t know, but head outside anyway and report back."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 60 && < 70: 
+      var colorTextArr = ["Do you need a jacket? Do you wear a sweater? A t-shirt? Who the fuck knows."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 50 && < 60: 
+      var colorTextArr = ["Today’s weather is amazing so get your butt outside already.",
+      "It’s rainbows and unicorns and fairies out there."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 40 && < 50:
+      var colorTextArr = ["Wow, this is perfect weather for a beer. Jk, it’s always perfect weather for a beer."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 30 && < 40: 
+      var colorTextArr = ["Wow, this is perfect weather for a beer. Jk, it’s always perfect weather for a beer."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp >= 20 && < 30:
+      var colorTextArr = ["It’s too cold to leave bed. Don’t bother.",
+      "Today’s weather means your hands are colder than the beer you’re drinking. Worth it."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    case temp < 20:
+      var colorTextArr = ["It’s Summer! ...somewhere else. Far away. Not here.",
+      "Today’s weather is colder than the cold, icy heart you pretend you don’t have.",
+      "Today is the cold, bleak grip of a death eater. All day."]
+      
+      var random = Math.floor(Math.random() * colorTextArr)
+      var weatherText = colorTextArr[random];
+      break;
+    
+    default: 
+      break;
+  }
+
+  console.log(weatherText);
+  writeToThePage(weatherText);
+}
+
+function writeToThePage(text) {
+  var pColorText = 
 }
 
 ////////// Initializes Firebase
